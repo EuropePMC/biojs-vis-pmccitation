ba344445e1a9b2bbb52ee2dd98235cc107487842
Sebastian Wilzbach <sebi@wilzbach.me>
2014-11-18 21:08:25 +0100

    initial commit

---
+++ snippets/citation.js
@@ -0,0 +1,16 @@
+var app = require("biojs-vis-pmccitation");
+
+var instance = new app.Citation({
+  target: yourDiv.id,
+  source: app.Citation.MED_SOURCE,
+  citation_id: '23962577',
+  width: 400,
+  proxyUrl: 'https://cors-anywhere.herokuapp.com/',
+  displayStyle: app.Citation.FULL_STYLE,
+  elementOrder: app.Citation.TITLE_FIRST,
+  showAbstract: true
+});	
+
+//triggers the citation data loading process that will use the Europe PMC RESTFUL Web service
+instance.loadCitation();
+
---
+++ snippets/citationlist.js
@@ -0,0 +1,13 @@
+var app = require("biojs-vis-pmccitation");
+
+var instance = new app.CitationList({
+  target: yourDiv.id,
+  query:'p53',
+  width: 400,
+  numberResults: 10,
+  displayStyle: app.CitationList.FULL_STYLE,
+  elementOrder: app.CitationList.TITLE_FIRST
+});	
+
+//triggers the citation data loading process that will use the Europe PMC RESTFUL Web service
+instance.loadResults();
